b64bc57e65cbed0497c78cfca9c9e43dc3eb0e23
Coleoptera <66390465+digitalmirrorCole@users.noreply.github.com>
2025-10-16 22:18:46 -0500

    Update docker-start.sh

--- docker-start.sh
+++ docker-start.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 set -e
-
-echo "Running alembic upgrade"
-alembic upgrade head
-
-echo "Running uvicorn"
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker app.main:app --bind 0.0.0.0:${PORT:-8000}
